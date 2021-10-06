4a4aec7fc1eaa63102918a10080220313e52bc19
三爷 <allenlikeu@gmail.com>
2021-10-06 12:00:17 +0800

    init: java version

---
+++ src/main/java/zone/motionless/houseanalyzer/HouseanalyzerApplication.java
@@ -0,0 +1,13 @@
+package zone.motionless.houseanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseanalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HouseanalyzerApplication.class, args);
+	}
+
+}
